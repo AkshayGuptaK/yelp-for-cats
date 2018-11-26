d94c9a4435fee409a2a25921b9b69cfd6b235686
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-26 10:32:01 +0530

    Webpack & Babel toolchain

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env'] } // not sure if needed
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/dist/'),
+    // publicPath: '/public/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
